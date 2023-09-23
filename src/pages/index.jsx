1020fcf24cc9627671eb8ef4dee1678c505039aa
eyesofdaveed <davdauit@gmail.com>
2023-09-23 11:44:18 +0600

    add fb pixels

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -30,6 +30,22 @@ const HomeAppLandingOnePage = () => {
     <>
       <Head>
         <title>Smavy - Курсы программирования в новом формате</title>
+        <meta
+          name="facebook-domain-verification"
+          content="f4tnwnb6fxv87swnh6yftfhlvrsgxv"
+        />
+        <script>
+          !function(f,b,e,v,n,t,s) if(f.fbq)return;n=f.fbq=function()
+          {n.callMethod
+            ? n.callMethod.apply(n, arguments)
+            : n.queue.push(arguments)}
+          ; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js'); fbq('init',
+          '134078346421134'); fbq('track', 'PageView');
+        </script>
       </Head>
 
       <MainLayout>
