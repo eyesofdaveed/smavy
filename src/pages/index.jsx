6532c4b8f5927a1695a185fbbd787aff79f80990
eyesofdaveed <davdauit@gmail.com>
2023-09-23 11:52:26 +0600

    fb pixel

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 import Head from "next/head";
 //= Scripts
 import navbarScrollEffect from "@common/navbarScrollEffect";
+import Script from "next/script";
 //= Layout
 import MainLayout from "@layouts/Main";
 //= Components
@@ -28,24 +29,25 @@ const HomeAppLandingOnePage = () => {
 
   return (
     <>
+      <Script id="facebook-pixel">
+        {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '134078346421134');
+fbq('track', 'PageView');
+`}
+      </Script>
       <Head>
         <title>Smavy - Курсы программирования в новом формате</title>
         <meta
           name="facebook-domain-verification"
           content="f4tnwnb6fxv87swnh6yftfhlvrsgxv"
         />
-        <script>
-          !function(f,b,e,v,n,t,s) if(f.fbq)return;n=f.fbq=function()
-          {n.callMethod
-            ? n.callMethod.apply(n, arguments)
-            : n.queue.push(arguments)}
-          ; if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js'); fbq('init',
-          '134078346421134'); fbq('track', 'PageView');
-        </script>
       </Head>
 
       <MainLayout>
